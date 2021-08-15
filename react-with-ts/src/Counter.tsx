7fcec19e0b225725ae497acbd3c77f1bc899fdc4
Bernese-Corgi <jnyng0621@gmail.com>
2021-08-15 15:49:09 +0900

    타입스크립트와 리액트 Counter 예제

---
+++ react-with-ts/src/Counter.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // generics를 사용하여 상태가 어떤 타입을 가질지 지정
+  // const [count, setCount] = useState<number>(0);
+  // generics를 사용하지 않아도 타입을 유추할 수는 있다.
+  const [count, setCount] = useState(0);
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={onIncrease}>+1</button>
+        <button onClick={onDecrease}>-1</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
